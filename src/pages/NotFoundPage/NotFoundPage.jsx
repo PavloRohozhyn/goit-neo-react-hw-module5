99215169dcdc40c2015aed9ab6672d7d6988ee52
Pavlo <pavlo.rohozhyn@gmail.com>
2025-05-26 09:06:35 +0200

    not found component - fix

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const NotFoundPage = () => {
-  return <p>404, Page Not Found.</p>;
+  return (
+    <>
+      <p>404, Page Not Found.</p>
+      <Link to="/">Home</Link>
+    </>
+  );
 };
 
 export default NotFoundPage;
